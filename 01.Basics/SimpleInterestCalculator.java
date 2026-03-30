285f8b8f71db18b6067f3e594dadd89f40c46145
Nirbhay Giri <nirbhaygiri2006@gmail.com>
2026-03-30 19:09:57 +0530

    Added new java Programs

---
+++ 01.Basics/SimpleInterestCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SimpleInterestCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Principal: ");
+        float p = sc.nextFloat();
+
+        System.out.print("Enter Rate: ");
+        float r = sc.nextFloat();
+
+        System.out.print("Enter Time: ");
+        float t = sc.nextFloat();
+
+        float si = (p * r * t) / 100;
+
+        System.out.println("Simple Interest = " + si);
+        sc.close();
+    }
+}
